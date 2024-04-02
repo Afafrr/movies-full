ceb462be19ee876aacbae93d9bfab856fb8265f6
Afafrr <mateusz.aftanski@gmail.com>
2024-04-02 10:54:35 +0200

    changed config to another account

--- src/services/config/firebase.ts
+++ src/services/config/firebase.ts
@@ -3,15 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDERfXyHznbJU4AJ06RRnZxbwpPkg4MNj0",
-//   authDomain: "movies-app-192fe.firebaseapp.com",
-//   projectId: "movies-app-192fe",
-//   storageBucket: "movies-app-192fe.appspot.com",
-//   messagingSenderId: "349635471278",
-//   appId: "1:349635471278:web:f9b7a4a39e68b2c80d4a3f",
-//   measurementId: "G-S20TZL3BKP",
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyAepZX-fe6bq4k8xrgnEu83vabl29WpBIg",
   authDomain: "movies-app-26923.firebaseapp.com",
